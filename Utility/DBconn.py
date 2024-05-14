3d67587ffb08888a8dc8b85c6c73a31817b0f866
Gnanaguru18 <gnanaguru2003is@gmail.com>
2024-05-14 14:38:01 +0530

    separeted DBconn to util

--- Utility/DBconn.py
+++ Utility/DBconn.py
@@ -1,20 +1,10 @@
 import pyodbc
+from Utility.DBPropertyUtil import PropertyUtil
 
-server_name = "TIGGER\\SQLEXPRESS"
-database_name = "HexawareMoviesDB"
- 
- 
-conn_str = (
-    f"Driver={{SQL Server}};"
-    f"Server={server_name};"
-    f"Database={database_name};"
-    f"Trusted_Connection=yes;"
-)
-
-print(conn_str)
 
 class DBconnection:
     def __init__(self):
+        conn_str=PropertyUtil.get_property_string()
         self.conn=pyodbc.connect(conn_str)
         self.cursor=self.conn.cursor()
     
